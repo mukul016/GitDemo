5f9febc412e5cfac230cc8b344b01a2d3d1c33fb
mukul <mukul.jain011@gmail.com>
2021-01-01 09:05:15 -0800

    Develop commit -2

--- automation/src/main/java/AutoFramework/automation/DevelopBranch.java
+++ automation/src/main/java/AutoFramework/automation/DevelopBranch.java
@@ -4,6 +4,10 @@ public class DevelopBranch {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println( "Happ new year" );
+        System.out.println( "Happy New Year" );
+        System.out.println( "Happ new year" );
+        System.out.println( "Happy New Year" );
 
 	}
 
