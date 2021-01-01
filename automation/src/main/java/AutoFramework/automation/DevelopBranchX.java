0659883001eece7eeb404706cc35628f910441b6
mukul <mukul.jain011@gmail.com>
2021-01-01 09:36:39 -0800

    X Commit

--- automation/src/main/java/AutoFramework/automation/DevelopBranchX.java
+++ automation/src/main/java/AutoFramework/automation/DevelopBranchX.java
@@ -4,7 +4,10 @@ public class DevelopBranchX {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println( "X Factor" );
+        System.out.println( "X Factor" );
+        System.out.println( "X Factor" );
+        System.out.println( "X Factor" );
 	}
 
 }
