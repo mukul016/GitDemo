ec4c736dafad6c8f71f08ce46aa6d188f9e57639
mukuljain <mukul.jain011@gmail.com>
2023-03-20 16:44:27 +0530

    commit from master branch

--- automation/src/main/java/AutoFramework/automation/App.java
+++ automation/src/main/java/AutoFramework/automation/App.java
@@ -8,6 +8,12 @@ public class App
 {
     public static void main( String[] args )
     {
+    	
+    	System.out.println( "Master branch" );
+        System.out.println( "Master Branch" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
         System.out.println( "Hello World!" );
         System.out.println( "Hello World!" );
         System.out.println( "Hello World!" );
